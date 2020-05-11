b70fde1d16542df7df8db62b7107b91132f8aa7e
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-05-11 22:25:04 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('message', message => {
          * There are big differences between a user and a member
          */
         member
-          .message.channel.send('f')
+          .send('f')
           .then(() => {
             // We let the message author know we were able to kick the person
             message.reply(`Successfully slapped ${user.tag}`);
