b6866b7c54e941e6bb993348917a8b597fad807a
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-05-11 22:21:07 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-
 client.on('ready', () => {
 
     console.log('I am ready!');
@@ -11,19 +10,49 @@ client.on('ready', () => {
 });
 
 
-
 client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
 
-    if (message.content === '!summon') {
-
-       message.reply('I have been summoned');
-
-       }
-
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!slap')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .message.channel.send('f')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully slapped ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+  }
 });
-
-
-
-// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
