58d86b66f26324660a60e9822ab106c13ff2cfed
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-05-12 21:13:59 +1000

    asdasdsadasd

--- bot.js
+++ bot.js
@@ -2,20 +2,28 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+var fs = require("fs"),
+    path = require("path"),
+    util = require("util");
+var content;
+console.log(content);
+fs.readFile(path.join(__dirname, "fun.txt"), 'utf8', function (err, data) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    content = util.format(data, "test", "test", "test");
+    console.log(content);
+});
 
 client.on('ready', () => {
 
     console.log('I am ready!');
+      let text = content;
+      msg.guild.members.forEach(member => {
+      if (member.id != client.user.id && !member.user.bot) member.send(text);
+      });
 
-});
-
-client.on('message', msg => {
-if (msg.guild && msg.content.startsWith('/private')) {
-let text = msg.content.slice('/private'.length); // cuts off the /private part
-msg.guild.members.forEach(member => {
-if (member.id != client.user.id && !member.user.bot) member.send(text);
-});
-}
 });
 
 client.login(process.env.BOT_TOKEN);
