b2088872727c1380b83bdc31a1ff204b92ff64d3
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-05-12 19:42:11 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,22 +34,5 @@ client.on('message', message => {
       message.reply("You didn't mention the user to slap!");
     }
   }
-const filter = (reaction, user) => {
-	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
 };
-message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-	.then(collected => {
-		const reaction = collected.first();
-
-		if (reaction.emoji.name === '👍') {
-			message.reply('you reacted with a thumbs up.');
-		} else {
-			message.reply('you reacted with a thumbs down.');
-		}
-	})
-	.catch(collected => {
-		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
-	});
-});
-
 client.login(process.env.BOT_TOKEN);
