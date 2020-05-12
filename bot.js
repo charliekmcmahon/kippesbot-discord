15af3e215078413e01fd8620facb06bf07d303f7
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-05-12 19:21:30 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,6 +38,25 @@ client.on('message', message => {
     const emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'ok');
     message.react(emoji);
   }
+message.react('👍').then(() => message.react('👎'));
+
+const filter = (reaction, user) => {
+	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+};
+
+message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '👍') {
+			message.reply('you reacted with a thumbs up.');
+		} else {
+			message.reply('you reacted with a thumbs down.');
+		}
+	})
+	.catch(collected => {
+		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	});
 });
 
 client.login(process.env.BOT_TOKEN);
